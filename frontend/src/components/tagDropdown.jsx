e6d2f024c44dc3d1e3a248906c4d309a92a2b71d
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-07-25 18:28:57 +0900

    드롭다운 구현
    드롭다운 제거는 구현 보류

--- frontend/src/components/tagDropdown.jsx
+++ frontend/src/components/tagDropdown.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import React, { useState } from "react";
+import Tag from "./tag";
+
 import PolyGon from "../img/Polygon2.png";
 
 const DropdownContainer = styled.div`
+  position: relative;
   width: 24.8vw;
   height: 3.2vw;
-  margin-bottom: 3.7vw;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -19,17 +22,80 @@ const DropdownButton = styled.div`
   cursor: pointer;
 `;
 
+const DropDownMenu = styled.div`
+  display: ${(props) => (props.show ? "block" : "none")};
+  background-color: white;
+  border: 0.05vw solid #ccc;
+  border-radius: 0.8vw;
+  margin-top: 0.5vw;
+  width: 100%;
+  z-index: 1;
+  box-shadow: 0 0.4vw 0.8vw rgba(0, 0, 0, 0.2);
+  position: absolute;
+  left: 24vw;
+  top: 1.5vw;
+`;
+
+const DropDownItem = styled.div`
+  padding: 1vw;
+  cursor: pointer;
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
+
+const TagList = styled.div`
+  display: flex;
+  margin-bottom: 3.7vw;
+  gap: 0.5vw;
+`;
+
 function TagDropdown({ tags, setTags }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const availableTags = [
+    { name: "Swift", color: "#6630ff" },
+    { name: "Ios", color: "#FF3F3F" },
+    { name: "Java", color: "#00A775" },
+  ];
+
+  const handleTagSelect = (tag) => {
+    const isTagSelected = tags.some((t) => t.name === tag.name);
+    if (!isTagSelected && tags.length < 5) {
+      setTags([...tags, tag]);
+      setShowDropdown(false);
+    }
+  };
+  console.log(tags);
+
   return (
     <>
-      <DropdownContainer>
+      <DropdownContainer onClick={() => setShowDropdown(!showDropdown)}>
         <DropdownInput placeholder="태그를 선택해 주세요. (최대 5개)"></DropdownInput>
         <DropdownButton>
           <img src={PolyGon} style={{ width: "1.4vw", height: "1.0vw" }} />
         </DropdownButton>
+        <DropDownMenu show={showDropdown}>
+          {availableTags.map((tag, index) => (
+            <DropDownItem key={index} onClick={() => handleTagSelect(tag)}>
+              {tag.name}
+            </DropDownItem>
+          ))}
+        </DropDownMenu>
       </DropdownContainer>
+      <TagList>
+        {tags.map((tag, index) => (
+          <Tag
+            key={index}
+            name={tag.name}
+            color={tag.color}
+            onClick={() => {
+              //드롭다운 제거(구현보류)
+              setTags(tags.filter((t) => t.name !== tag.name));
+            }}
+          ></Tag>
+        ))}
+      </TagList>
     </>
   );
 }
-
 export default TagDropdown;
