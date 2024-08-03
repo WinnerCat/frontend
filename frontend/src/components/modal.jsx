11bddd18370702556f4ac4a54979fe7b0ae6ec44
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-08-04 00:08:27 +0900

    모달 추가

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Frame from "../img/Frame5.png";
 
 const Container = styled.div`
   width: 100%;
@@ -13,22 +14,26 @@ const Container = styled.div`
 `;
 
 const ModalContainer = styled.div`
-  width: 25vw;
-  height: 10.8vw;
+  width: 35vw;
+  height: 20.8vw;
   flex-shrink: 0;
   border-radius: 0.8vw;
   background: #fff;
   position: relative;
-  left: 40%;
-  top: 40%;
+  left: 34%;
+  top: 32%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.div`
   color: #000;
   font-family: Inter;
-  font-size: 1vw;
+  font-size: 1.5vw;
   font-style: normal;
-  font-weight: 400;
+  font-weight: 700;
   line-height: 220%; /* 44px */
   display: flex;
   justify-content: center;
@@ -38,6 +43,9 @@ const Title = styled.div`
 
 const ButtonContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5vw;
   width: 100%;
   height: 50%;
 `;
@@ -46,16 +54,17 @@ const YesButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #000;
-  font-family: Inter;
+  border-radius: 1vw;
+  background: var(--6630FF, #6630ff);
+  width: 4.5vw;
+  height: 2.2vw;
+  flex-shrink: 0;
+  color: var(--FFFFFF, #fff);
+  font-family: Pretendard;
   font-size: 1vw;
   font-style: normal;
-  font-weight: 400;
-  line-height: 220%; /* 44px */
-  width: 50%;
-  height: 70%;
-  border-top: solid 0.05vw #000;
-  border-right: solid 0.05vw #000;
+  font-weight: 700;
+  line-height: 0%; /* 0px */
   cursor: pointer;
 `;
 
@@ -63,16 +72,36 @@ const NoButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #000;
-  font-family: Inter;
+  border-radius: 1vw;
+  width: 4.5vw;
+  height: 2.2vw;
+  border: 0.05vw solid var(--6630FF, #6630ff);
+  cursor: pointer;
+  color: var(--6630FF, #6630ff);
+  font-family: Pretendard;
+  font-size: 1vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 0%; /* 0px */
+`;
+
+const ImgContainer = styled.div`
+  width: 2.3vw;
+  height: 2.3vw;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2vw;
+`;
+
+const SubTitle = styled.div`
+  color: var(--808080, #808080);
+  font-family: Pretendard;
   font-size: 1vw;
   font-style: normal;
   font-weight: 400;
-  line-height: 220%; /* 44px */
-  width: 50%;
-  height: 70%;
-  border-top: solid 0.05vw #000;
-  cursor: pointer;
+  line-height: 220%; /* 33px */
 `;
 
 function Modal({ isOpen, closeModal, title }) {
@@ -82,10 +111,14 @@ function Modal({ isOpen, closeModal, title }) {
     <>
       <Container style={{ display: isOpen ? "block" : "none" }}>
         <ModalContainer>
+          <ImgContainer>
+            <img src={Frame} style={{ width: "100%", height: "100%" }} />
+          </ImgContainer>
           <Title>{title}</Title>
+          <SubTitle>같은 버그를 겪고 있는 개발자가 있을수도 있어요!</SubTitle>
           <ButtonContainer>
-            <YesButton onClick={() => navigate("/")}>네</YesButton>
-            <NoButton onClick={closeModal}>아니요</NoButton>
+            <NoButton onClick={closeModal}>취소</NoButton>
+            <YesButton onClick={() => navigate("/")}>확인</YesButton>
           </ButtonContainer>
         </ModalContainer>
       </Container>
